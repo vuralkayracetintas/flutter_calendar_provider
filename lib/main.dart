b76bc2390b5360e92aba632930979ed1ee8408bd
Vural Kayra Cetintas <73638860+vuralkayracetintas@users.noreply.github.com>
2023-09-08 11:28:49 +0300

    login screens , container

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
+import 'package:demo_apps/login_pages/login_screen_ui_2.dart';
 import 'package:demo_apps/provider/event_provider.dart';
-import 'package:demo_apps/qrcode.dart';
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -7,18 +8,20 @@ void main() {
   runApp(
     ChangeNotifierProvider(
       create: (context) => EventProvider(),
-      child: MyApp(),
+      child: const MyApp(),
     ),
   );
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'My App',
-      home: QrCodeCreatePage(),
+      home: LoginPage(),
     );
   }
 }
