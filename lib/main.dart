1717a122fe1bf8fa4ea25a0be6cc5fc8b25c65cb
Vural Kayra Cetintas <73638860+vuralkayracetintas@users.noreply.github.com>
2023-09-24 00:07:48 +0300

    qr code scanner

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:demo_apps/bottomSheet/bottom_sheet_view.dart';
 import 'package:demo_apps/provider/event_provider.dart';
+import 'package:demo_apps/qrcode/qrcode_scanner.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark(useMaterial3: true),
       title: 'My App',
-      home: BottomSheetView(),
+      home: QrCodeScanner(),
     );
   }
 }
