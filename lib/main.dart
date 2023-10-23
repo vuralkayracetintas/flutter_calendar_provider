6a2c8a531f33237276b6a7f5827fd6915f595a29
vuralkayracetintas <vuralkayracetintass@gmail.com>
2023-10-23 23:05:49 +0300

    getx snackbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,5 @@
-import 'package:demo_apps/animation/lotie_demo.dart';
-
-import 'package:demo_apps/animation/orbit_animation/orbit.dart';
-
-import 'package:demo_apps/animation/orbit_animation/orbit_animation.dart';
 import 'package:demo_apps/getx_learn/getx_main.dart';
-import 'package:demo_apps/page_controller/page_controller_view.dart';
 import 'package:demo_apps/provider/event_provider.dart';
-import 'package:demo_apps/qrcode/qrcode_scanner.dart';
-
-import 'package:demo_apps/singlechild_button/view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/route_manager.dart';
 import 'package:provider/provider.dart';
